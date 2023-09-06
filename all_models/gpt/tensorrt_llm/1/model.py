3cd8851b564ad3d2c812943a3fb78078448a9cf9
Tao Li <tali@nvidia.com>
2023-09-05 21:48:32 -0700

    Patch for aug release

--- all_models/gpt/tensorrt_llm/1/model.py
+++ all_models/gpt/tensorrt_llm/1/model.py
@@ -1,11 +1,12 @@
 import json
 import os
 
-import tensorrt_llm
 import torch
 import triton_python_backend_utils as pb_utils
-from tensorrt_llm.runtime import GenerationSession, ModelConfig, SamplingConfig
 from torch import from_numpy
+
+import tensorrt_llm
+from tensorrt_llm.runtime import GenerationSession, ModelConfig, SamplingConfig
 
 
 def mpi_comm():
