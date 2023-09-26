e64dd599a80402a5cb66c1cf85bce0161898cac7
Stanley Sun <stsun@nvidia.com>
2023-09-26 01:02:09 -0700

    Add inflight batching perf test cases

--- tests/llm-backend-test-defs/turtle/defs/perf/test_perf.py
+++ tests/llm-backend-test-defs/turtle/defs/perf/test_perf.py
@@ -26,9 +26,9 @@ class PerfTestConfig:
     def __init__(self,
                  model_name: str,
                  metrics: Dict[Metric, float],
-                 batch_size: int = 1,
-                 input_len: int = 128,
-                 output_len: int = 8,
+                 batch_size: int = -1,
+                 input_len: int = -1,
+                 output_len: int = -1,
                  num_runs: int = 10):
         self.model_name = model_name
         self.metrics = metrics
@@ -163,5 +163,153 @@ def test_perf(turtle_case_name, trt_performance_cache_fpath,
 
     case.set_runtime_configs(benchmark_script, working_dir,
                              trt_performance_cache_fpath)
+    case.run_metrics(turtle_case_name, llm_backend_venv, trt_gpu_clock_lock,
+                     llm_session_data_writer, output_dir)
+
+
+class InflightBatchingPerfTestConfig:
+    """
+    Configurations defining the LLM Backend perf test.
+    """
+
+    def __init__(self,
+                 model_name: str,
+                 metrics: Dict[Metric, float],
+                 concurrency: int = -1,
+                 max_input_len: int = -1):
+        self.model_name = model_name
+        self.metrics = metrics
+        self.concurrency = concurrency
+        self.max_input_len = max_input_len
+
+    def __str__(self):
+        entries = [
+            self.model_name, f"concurrency:{self.concurrency}",
+            f"max_input_len:{self.max_input_len}"
+        ]
+
+        return "-".join(entries)
+
+
+class InflightBatchingMetricPerfTest(AbstractPerfScriptTestClass):
+    """
+    Base class for perf tests with multiple metrics.
+    """
+
+    def __init__(self, config: InflightBatchingPerfTestConfig):
+        assert isinstance(config, InflightBatchingPerfTestConfig)
+        self._config = config
+        self._current_metric = None
+
+    def get_test_name(self) -> str:
+        return str(self._config)
+
+    def set_runtime_configs(self, benchmark_script, working_dir,
+                            perf_cache_fpath, dataset, tokenizer_dir,
+                            tokenizer_type) -> None:
+        self._benchmark_script = benchmark_script
+        self._working_dir = working_dir
+        self._perf_cache_fpath = perf_cache_fpath
+        self._dataset = dataset
+        self._tokenizer_dir = tokenizer_dir
+        self._tokenizer_type = tokenizer_type
+
+    def get_commands(self) -> List[PerfScriptTestCmd]:
+        benchmark_cmd = [
+            self._benchmark_script,
+            f"--concurrency={self._config.concurrency}",
+            f"--max_input_len={self._config.max_input_len}",
+            f"--dataset={self._dataset}",
+            f"--tokenizer_dir={self._tokenizer_dir}",
+            f"--tokenizer_type={self._tokenizer_type}"
+        ]
+
+        benchmark_cmd = PerfScriptTestCmd(benchmark_cmd, isPython=True)
+
+        return [benchmark_cmd]
+
+    def get_perf_result(self, outputs: List[str]) -> float:
+        metric = self._current_metric
+        # check perf time
+        perf_times = [
+            metric.regexp.search(line) for line in outputs[0].split("\n")
+        ]
+        perf_times = [float(match.group(1)) for match in perf_times if match]
+
+        if len(perf_times) == 1:
+            return perf_times[0]
+
+        raise RuntimeError("Cannot find perf result from perf script logs!")
+
+    def get_threshold(self) -> float:
+        return self._config.metrics[self._current_metric]
+
+    def get_metric_type(self) -> PerfMetricType:
+        return self._current_metric.tp
+
+    def run_metrics(self, turtle_case_name, llm_backend_venv, gpu_clock_lock,
+                    trt_session_data_writer, output_dir):
+        outputs = []
+        for metric in self._config.metrics:
+            self._current_metric = metric
+            fullname = turtle_case_name.replace(
+                "test_perf",
+                f"test_perf_metric_{self._current_metric.tp.lower()}")
+            try:
+                outputs = self.run_ex(fullname,
+                                      llm_backend_venv,
+                                      gpu_clock_lock,
+                                      trt_session_data_writer,
+                                      output_dir,
+                                      outputs=outputs)
+            except Exception as e:
+                print(f"perf command failed with {e}")
+
+
+CONCURRENCY = [2, 4, 8, 16]
+MAX_INPUT_LEN = [300, 400, 500]
+
+
+def generate_llm_ib_perf_config():
+    configs = []
+    for concurrency in CONCURRENCY:
+        for max_input_len in MAX_INPUT_LEN:
+            for model in ['llama']:
+                configs.append(
+                    InflightBatchingPerfTestConfig(
+                        model,
+                        {
+                            LATENCY: 0.1,
+                        },
+                        concurrency=concurrency,
+                        max_input_len=max_input_len,
+                    ))
+    return configs
+
+
+# Generate test list and test name list.
+LLM_IB_TESTS = [
+    InflightBatchingMetricPerfTest(config)
+    for config in generate_llm_ib_perf_config()
+]
+
+
+@pytest.mark.parametrize("case", LLM_IB_TESTS, ids=lambda c: c.get_test_name())
+def test_perf_inflight_batching(turtle_case_name, trt_performance_cache_fpath,
+                                trt_gpu_clock_lock, llm_session_data_writer,
+                                output_dir, llm_backend_venv, llm_backend_root,
+                                case, llama_v2_tokenizer_model_root):
+    """
+    The actual test definition for TensorRT LLM Backend perf test.
+    """
+    benchmark_script = os.path.join(llm_backend_root, "tools",
+                                    "inflight_batcher_llm", "identity_test.py")
+    dataset_path = os.path.join(llm_backend_root, "tools", "dataset",
+                                "mini_cnn_eval.json")
+    working_dir = llm_backend_venv.get_working_directory()
+    tokenizer_type = "llama"
+    case.set_runtime_configs(benchmark_script, working_dir,
+                             trt_performance_cache_fpath, dataset_path,
+                             llama_v2_tokenizer_model_root, tokenizer_type)
     case.run_metrics(turtle_case_name, llm_backend_venv, trt_gpu_clock_lock,
                      llm_session_data_writer, output_dir)
