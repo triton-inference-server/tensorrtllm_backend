f3e211922c943abbe3e22a1e8ff9ffd712766841
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2023-11-17 07:34:51 -0800

    Update TensorRT-LLM backend

--- scripts/benchmarking/test.sh
+++ scripts/benchmarking/test.sh
@@ -5,8 +5,9 @@ ENGINE_PATH=$2
 TOKENIZER_PATH=$3
 TOKENIZER_TYPE=$4
 BS=$5
-WORLD_SIZE=$6
-RECORD_LOG=$7
+MAX_INPUT_SEQLEN=$6
+WORLD_SIZE=$7
+RECORD_LOG=$8
 
 set -e
 nvidia-smi
@@ -16,27 +17,64 @@ source tools/utils.sh
 
 #-----------------------  WORKLOAD_DETAILS -----------------------#
 
-# token normal dist.
+# token normal distribution.
 # (ip_mean, ip_stdev, op_mean, op_stdev, num_prompts)
-TOKEN_DIST_LIST=( "128,10,20,2,500" "20,2,128,10,500" )
-DATASETS=( "cnn" ) # names of datasets
+TOKEN_DIST_LIST=( "128,10,20,2,5000" "20,2,128,10,5000" )
+DATASETS=( "<dataset>" ) # names of datasets
 
 # key: dataset name, value: path to dataset json file
-declare -A dataset_dict=( []"cnn"]="<dataset_path>" )
+declare -A dataset_dict=( ["<dataset>"]="<dataset_path>" )
 
-# dictionary[workload] =  list of request rates . Should contain keys from TOKEN_DIST_LIST and DATASETS
-declare -A REQ_RATES=(  ["128,10,20,2,500"]="-1,20"
-                        ["20,2,128,10,500"]="-1,40"
-                        ["cnn"]="-1,60"
+# dictionary[workload] =  list of request rates to shmoo over. Should contain keys from TOKEN_DIST_LIST and DATASETS
+declare -A REQ_RATES=(  ["128,10,20,2,5000"]="-1"
+                        ["20,2,128,10,5000"]="-1"
+                        ["cnn"]="-1"
                     )
+REQ_RATES_HIST="-1" #
 #-----------------------------------------------------------------#
 
+EXCLUDE_INPUT_IN_OUTPUT="false"
+ENABLE_TRT_OVERLAP="false"
+MAX_QUEUE_DELAY_MICROSECONDS="0"
+MAX_BEAM_WIDTH="1"
+
+gpu_info=$(nvidia-smi --query-gpu=name --format=csv,noheader,nounits)
+if [[ $gpu_info == *"H100"* ]]; then
+    MACHINE="H100"
+elif [[ $gpu_info == *"A100"* ]]; then
+    MACHINE="A100"
+elif [[ $gpu_info == *"L40S"* ]]; then
+    MACHINE="L40S"
+fi
+
+fill_triton_repo () {
+    # Modify config.pbtxt
+    python3 tools/fill_template.py -i my_models/inflight_batcher_llm/tensorrt_llm/config.pbtxt engine_dir:${ENGINE_PATH},decoupled_mode:"False",batching_strategy:${BATCHING_STRATEGY},batch_scheduler_policy:${BATCH_SCHEDULER_POLICY},exclude_input_in_output:${EXCLUDE_INPUT_IN_OUTPUT},triton_max_batch_size:${BS},max_queue_delay_microseconds:${MAX_QUEUE_DELAY_MICROSECONDS},max_beam_width:${MAX_BEAM_WIDTH}
+    python3 tools/fill_template.py -i my_models/inflight_batcher_llm/preprocessing/config.pbtxt triton_max_batch_size:${BS},tokenizer_dir:${TOKENIZER_PATH},tokenizer_type:${TOKENIZER_TYPE}
+    python3 tools/fill_template.py -i my_models/inflight_batcher_llm/postprocessing/config.pbtxt triton_max_batch_size:${BS},tokenizer_dir:${TOKENIZER_PATH},tokenizer_type:${TOKENIZER_TYPE}
+    python3 tools/fill_template.py -i my_models/inflight_batcher_llm/ensemble/config.pbtxt triton_max_batch_size:${BS}
+
+}
+
+print_test_params () {
+
+    echo "----------------------------------"
+    echo " Test parameters:"
+    echo "----------------------------------"
+    echo "BATCHING_STRATEGY: ${BATCHING_STRATEGY}"
+    echo "BATCH_SCHEDULER_POLICY: ${BATCH_SCHEDULER_POLICY}"
+    echo "ENABLE_TRT_OVERLAP: ${ENABLE_TRT_OVERLAP}"
+    echo "EXCLUDE_INPUT_IN_OUTPUT: ${EXCLUDE_INPUT_IN_OUTPUT}"
+    echo "TRITON_MAX_BATCH_SIZE: ${BS}"
+    echo "MAX_QUEUE_DELAY_MICROSECONDS: ${MAX_QUEUE_DELAY_MICROSECONDS}"
+    echo "MAX_BEAM_WIDTH: ${MAX_BEAM_WIDTH}"
+    echo "----------------------------------"
+}
 
 if true; then
     echo "TRUE"
 
-    BATCHING_STRATEGIES=( "inflight_fused_batching" ) #"v1"
-
+    BATCHING_STRATEGIES=( "inflight_fused_batching" "v1" )
 
     for BATCHING_STRATEGY in "${BATCHING_STRATEGIES[@]}"; do
 
@@ -49,21 +87,17 @@ if true; then
         for BATCH_SCHEDULER_POLICY in "${BATCH_SCHEDULER_POLICIES[@]}"; do
 
             echo -e " \n ================= INITIALIZING TRITONSERVER FOR =============== \n"
-            echo "BATCH_SCHEDULER_POLICY: ${BATCH_SCHEDULER_POLICY}"
-            echo "BATCHING SCHEME: ${BATCHING_STRATEGY}"
+            print_test_params
 
             # Start each server with fresh configuration
             rm -rf my_models
             cp -R all_models my_models
 
-            # Modify config.pbtxt
-            python3 tools/fill_template.py -i my_models/inflight_batcher_llm/tensorrt_llm/config.pbtxt engine_dir:${ENGINE_PATH},decoupled_mode:"False",batching_strategy:${BATCHING_STRATEGY},batch_scheduler_policy:${BATCH_SCHEDULER_POLICY},exclude_input_in_output:"false",triton_max_batch_size:${BS},max_queue_delay_microseconds:"0",max_batch_width:"1"
-            python3 tools/fill_template.py -i my_models/inflight_batcher_llm/preprocessing/config.pbtxt triton_max_batch_size:${BS},tokenizer_dir:${TOKENIZER_PATH},tokenizer_type:${TOKENIZER_TYPE}
-            python3 tools/fill_template.py -i my_models/inflight_batcher_llm/postprocessing/config.pbtxt triton_max_batch_size:${BS},tokenizer_dir:${TOKENIZER_PATH},tokenizer_type:${TOKENIZER_TYPE}
+            fill_triton_repo
 
             if [ "$RECORD_LOG" == "true" ]; then
                 echo -e " \n ========= Collecting log for the server ======== \n"
-                python3 scripts/launch_triton_server.py --world_size $WORLD_SIZE --model_repo my_models/inflight_batcher_llm/ --tritonserver "/opt/tritonserver/bin/tritonserver --log-verbose 3 --log-file triton_log.txt"
+                python3 scripts/launch_triton_server.py --world_size $WORLD_SIZE --model_repo my_models/inflight_batcher_llm/ --log --log-file triton_log.txt
             else
                 python3 scripts/launch_triton_server.py --world_size $WORLD_SIZE --model_repo my_models/inflight_batcher_llm/
             fi
@@ -82,18 +116,6 @@ if true; then
             pushd tools/inflight_batcher_llm/
             if [ $? -eq 0 ]; then
 
-                if [[ "$MODEL" == *"fp8"* ]]; then
-                    MACHINE="h100"
-                else
-                    MACHINE="a100"
-                fi
-
-                if [[ "$MODEL" == *"gptj"* ]]; then
-                    MAX_INPUT_LEN=1535
-                else
-                    MAX_INPUT_LEN=128000
-                fi
-
                 for DATASET in "${DATASETS[@]}"; do
                     IFS=',' read -ra REQUEST_RATES <<< "${REQ_RATES[${DATASET}]}"
                     for REQ_RATE in "${REQUEST_RATES[@]}"; do
@@ -101,13 +123,12 @@ if true; then
                         op_stats_csv_name="$op_stats_name.csv"
 
                         echo -e "DATASET: $DATASET \n\n"
-                        echo -e " ======== IDENTITY_TEST --> OP STATS FILE = ${op_stats_csv_name} ============== \n"
+                        echo -e " ======== BENCHMARK_CORE_MODEL --> OP STATS FILE = ${op_stats_csv_name} ============== \n"
                         dataset_path="${dataset_dict[$DATASET]}"
-                        # Identity test
-                        python3 identity_test.py \
-                            -i grpc --max_input_len $MAX_INPUT_LEN \
-                            --request_rate $REQ_RATE --op_stats_csv "$op_stats_csv_name" \
-                            --num_requests 3000 \
+                        python3 benchmark_core_model.py \
+                            -i grpc --max-input-len $MAX_INPUT_SEQLEN \
+                            --request-rate $REQ_RATE --op-stats-csv "$op_stats_csv_name" \
+                            --num-requests 3000 \
                             dataset \
                             --dataset $dataset_path \
                             --tokenizer_dir "$TOKENIZER_PATH" --tokenizer_type "$TOKENIZER_TYPE"
@@ -131,17 +152,32 @@ if true; then
                             op_stats_name="${MACHINE}__${MODEL}__${BATCHING_STRATEGY}__${BATCH_SCHEDULER_POLICY}__normal-token-dist-${ip_mean}-${ip_stdev}-${op_mean}-${op_stdev}__${REQ_RATE}"
                             op_stats_csv_name="$op_stats_name.csv"
                             echo -e "DATASET: normal-token-dist \n\n"
-                            echo -e " ======== IDENTITY_TEST --> OP STATS FILE = ${op_stats_csv_name} ============== \n"
-                            python3 identity_test.py \
-                                -i grpc --max_input_len $MAX_INPUT_LEN \
-                                --request_rate $REQ_RATE --op_stats_csv "$op_stats_csv_name" \
-                                --num_requests $num_prompts \
-                                token_norm_dist \
-                                --input_mean $ip_mean --input_stdev $ip_stdev --output_mean $op_mean --output_stdev $op_stdev \
+                            echo -e " ======== BENCHMARK_CORE_MODEL --> OP STATS FILE = ${op_stats_csv_name} ============== \n"
+                            python3 benchmark_core_model.py \
+                                -i grpc --max-input-len $MAX_INPUT_SEQLEN \
+                                --request-rate $REQ_RATE --op-stats-csv "$op_stats_csv_name" \
+                                --num-requests $num_prompts \
+                                token-norm-dist \
+                                --input-mean $ip_mean --input-stdev $ip_stdev --output-mean $op_mean --output-stdev $op_stdev \
 
 
                             sleep 5
                     done
+                done
+
+                IFS=',' read -ra REQUEST_RATES <<< $REQ_RATES_HIST
+                for REQ_RATE in "${REQUEST_RATES[@]}"; do
+
+                        op_stats_name="${MACHINE}__${MODEL}__${BATCHING_STRATEGY}__${BATCH_SCHEDULER_POLICY}__token-hist-example__${REQ_RATE}"
+                        op_stats_csv_name="$op_stats_name.csv"
+                        echo -e "DATASET: token-hist-example \n\n"
+                        echo -e " ======== BENCHMARK_CORE_MODEL --> OP STATS FILE = ${op_stats_csv_name} ============== \n"
+                        python3 benchmark_core_model.py \
+                            -i grpc --max-input-len $MAX_INPUT_SEQLEN \
+                            --request-rate $REQ_RATE --op-stats-csv "$op_stats_csv_name" \
+                            token-from-histogram --histogram-key example
+
+                        sleep 5
                 done
 
                 echo -e " \n ========= KILLING TRITON SERVER WITH PID:  #$SERVER_PID  ============== \n"
