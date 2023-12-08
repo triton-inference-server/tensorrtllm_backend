2ada5890a4daa653ca7ab96036be11d7b536611f
Kaiyu <26294424+kaiyux@users.noreply.github.com>
2023-12-08 01:03:18 -0800

    Update TensorRT-LLM backend

--- tools/gen_trtllm_dockerfile.py
+++ tools/gen_trtllm_dockerfile.py
@@ -51,12 +51,12 @@ RUN rm -fr tensorrtllm_backend
     COPY tensorrt_llm/docker/common/install_tensorrt.sh /tmp/
     """
     df += """
-ENV TRT_VER=9.1.0.4
-ENV CUDA_VER=12.2
-ENV CUDNN_VER=8.9.4.25-1+cuda12.2
-ENV NCCL_VER=2.18.3-1+cuda12.2
-ENV CUBLAS_VER=12.2.5.6-1
-RUN bash /tmp/install_tensorrt.sh && rm /tmp/install_tensorrt.sh
+
+ENV CUDA_VER=12.3
+ENV CUDNN_VER=8.9.6.50-1+cuda12.2
+ENV NCCL_VER=2.19.3-1+cuda12.3
+ENV CUBLAS_VER=12.3.2.9-1
+RUN bash /tmp/install_tensorrt.sh --CUDA_VER=$CUDA_VER --CUDNN_VER=$CUDNN_VER --NCCL_VER=$NCCL_VER --CUBLAS_VER=$CUBLAS_VER && rm /tmp/install_tensorrt.sh
 
 ENV LD_LIBRARY_PATH=/usr/local/tensorrt/lib/:$LD_LIBRARY_PATH
 ENV TRT_ROOT=/usr/local/tensorrt
