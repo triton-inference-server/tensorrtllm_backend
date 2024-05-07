e239adcfbf8e72bf1eefaa166781343f41770d70
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2024-05-07 23:37:15 +0800

    Update TensorRT-LLM backend (#444)

--- inflight_batcher_llm/src/libtensorrtllm.cc
+++ inflight_batcher_llm/src/libtensorrtllm.cc
@@ -31,8 +31,6 @@
 #include <memory>
 #include <thread>
 
-#include "tensorrt_llm/common/mpiUtils.h"
-
 // Triton headers
 #include "triton/backend/backend_common.h"
 #include "triton/core/tritonbackend.h"
@@ -41,57 +39,12 @@
 // trtllm backend headers
 #include "model_instance_state.h"
 #include "model_state.h"
-#include "orchestrator.h"
-#include "work_item.h"
-#include "work_items_queue.h"
-
-#ifdef TRITON_ENABLE_METRICS
-#include "custom_metrics_reporter/custom_metrics_reporter.h"
-#endif
 
 namespace triton::backend::inflight_batcher_llm
 {
 
 extern "C"
 {
-
-    // Global backend state creation
-    TRITONSERVER_Error* TRITONBACKEND_Initialize(TRITONBACKEND_Backend* backend)
-    {
-        tensorrt_llm::mpi::initialize(tensorrt_llm::mpi::MpiThreadSupport::THREAD_MULTIPLE);
-
-        char const* str = std::getenv("TRTLLM_ORCHESTRATOR");
-
-        if (str && std::atoi(str) != 0)
-        {
-            TLLM_LOG_INFO(
-                "Detected TRTLLM_ORCHESTRATOR environment variable, TRTLLM backend will operator in orchestrator "
-                "mode.");
-            auto* orchestrator = new Orchestrator();
-            RETURN_IF_ERROR(TRITONBACKEND_BackendSetState(backend, reinterpret_cast<void*>(orchestrator)));
-        }
-        else
-        {
-            RETURN_IF_ERROR(TRITONBACKEND_BackendSetState(backend, nullptr));
-        }
-
-        return nullptr; // success
-    }
-
-    TRITONSERVER_Error* TRITONBACKEND_Finalize(TRITONBACKEND_Backend* backend)
-    {
-        void* vstate;
-        RETURN_IF_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
-
-        if (vstate)
-        {
-            auto* orchestrator = reinterpret_cast<Orchestrator*>(vstate);
-            delete orchestrator;
-        }
-
-        return nullptr; // success
-    }
-
     // Triton calls TRITONBACKEND_ModelInitialize when a model is loaded
     // to allow the backend to create any state associated with the model,
     // and to also examine the model configuration to determine if the
@@ -147,43 +100,11 @@ extern "C"
         RETURN_IF_ERROR(TRITONBACKEND_ModelState(model, &vmodelstate));
         ModelState* model_state = reinterpret_cast<ModelState*>(vmodelstate);
 
-        TRITONBACKEND_Backend* backend;
-        RETURN_IF_ERROR(TRITONBACKEND_ModelBackend(model, &backend));
-
-        void* vstate;
-        RETURN_IF_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
-
-        auto* orchestrator = reinterpret_cast<Orchestrator*>(vstate);
-
-        if (orchestrator)
-        {
-            auto const device_ids = model_state->GetDeviceIds();
-            int const num_workers = device_ids ? device_ids.value().size() : 1;
-
-            std::string workerPath = model_state->GetWorkerPath();
-            MPI_Comm everyone;
-            MPI_Comm_spawn(workerPath.c_str(), MPI_ARGV_NULL, num_workers, MPI_INFO_NULL, 0, MPI_COMM_SELF, &everyone,
-                MPI_ERRCODES_IGNORE);
-
-            // The output comm is an intercommunicator so it has some special rules.
-            // The parent must send data with bcast using root = MPI_ROOT (-4)
-            std::vector<int64_t> packed = model_state->serialize();
-            int64_t n = packed.size();
-            MPICHECK(MPI_Bcast(&n, 1, MPI_INT64_T, MPI_ROOT, everyone));
-            MPICHECK(MPI_Bcast(packed.data(), packed.size(), MPI_INT64_T, MPI_ROOT, everyone));
-
-            OrchestratorCommunicator* communicator;
-            RETURN_IF_ERROR(orchestrator->addCommunicator(model_state, instance, everyone, &communicator));
-            RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceSetState(instance, reinterpret_cast<void*>(communicator)));
-        }
-        else
-        {
-            // Create a ModelInstanceState object and associate it with the
-            // TRITONBACKEND_ModelInstance.
-            ModelInstanceState* instance_state;
-            RETURN_IF_ERROR(ModelInstanceState::Create(model_state, instance, &instance_state));
-            RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceSetState(instance, reinterpret_cast<void*>(instance_state)));
-        }
+        // Create a ModelInstanceState object and associate it with the
+        // TRITONBACKEND_ModelInstance.
+        ModelInstanceState* instance_state;
+        RETURN_IF_ERROR(ModelInstanceState::Create(model_state, instance, &instance_state));
+        RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceSetState(instance, reinterpret_cast<void*>(instance_state)));
 
         return nullptr; // success
     }
@@ -197,27 +118,10 @@ extern "C"
         TRITONBACKEND_Model* model;
         RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceModel(instance, &model));
 
-        TRITONBACKEND_Backend* backend;
-        RETURN_IF_ERROR(TRITONBACKEND_ModelBackend(model, &backend));
-
         void* vstate;
-        RETURN_IF_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
-
-        auto* orchestrator = reinterpret_cast<Orchestrator*>(vstate);
-
         RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(instance, &vstate));
-
-        if (orchestrator)
-        {
-            auto* communicator = reinterpret_cast<OrchestratorCommunicator*>(vstate);
-            communicator->shutdown();
-            delete communicator;
-        }
-        else
-        {
-            ModelInstanceState* instance_state = reinterpret_cast<ModelInstanceState*>(vstate);
-            delete instance_state;
-        }
+        ModelInstanceState* instance_state = reinterpret_cast<ModelInstanceState*>(vstate);
+        delete instance_state;
 
         return nullptr; // success
     }
@@ -230,31 +134,10 @@ extern "C"
     TRITONSERVER_Error* TRITONBACKEND_ModelInstanceExecute(
         TRITONBACKEND_ModelInstance* instance, TRITONBACKEND_Request** requests, const uint32_t request_count)
     {
-        TRITONBACKEND_Model* model;
-        RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceModel(instance, &model));
+        ModelInstanceState* instance_state;
+        RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(instance, reinterpret_cast<void**>(&instance_state)));
 
-        TRITONBACKEND_Backend* backend;
-        RETURN_IF_ERROR(TRITONBACKEND_ModelBackend(model, &backend));
-
-        void* vstate;
-        RETURN_IF_ERROR(TRITONBACKEND_BackendState(backend, &vstate));
-
-        auto* orchestrator = reinterpret_cast<Orchestrator*>(vstate);
-
-        if (orchestrator)
-        {
-            OrchestratorCommunicator* communicator;
-            RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(instance, reinterpret_cast<void**>(&communicator)));
-
-            communicator->enqueue(requests, request_count);
-        }
-        else
-        {
-            ModelInstanceState* instance_state;
-            RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceState(instance, reinterpret_cast<void**>(&instance_state)));
-
-            instance_state->enqueue(requests, request_count);
-        }
+        instance_state->enqueue(requests, request_count);
 
         return nullptr; // success
     }
