83e67029d9f325ae1e118fc7f83f51473c9352a5
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2024-10-15 07:08:24 +0000

    open source ca3b8accfbb4de989b99a24c6ba55dd147041633

--- tools/fill_template.py
+++ tools/fill_template.py
@@ -3,11 +3,12 @@ from argparse import ArgumentParser
 from string import Template
 
 
-def main(file_path, substitutions, in_place):
+def main(file_path, substitutions, in_place, participant_ids):
     with open(file_path) as f:
         pbtxt = Template(f.read())
 
     sub_dict = {"max_queue_size": 0}
+    sub_dict["participant_ids"] = participant_ids
     for sub in substitutions.split(","):
         key, value = sub.split(":")
         sub_dict[key] = value
@@ -33,6 +34,9 @@ if __name__ == "__main__":
                         "-i",
                         action="store_true",
                         help="do the operation in-place")
+    parser.add_argument("--participant_ids",
+                        help="Participant IDs for the model",
+                        default="")
     args = parser.parse_args()
 
     main(**vars(args))
