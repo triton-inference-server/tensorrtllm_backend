ba6ddc92686b54690d382b6f7c70ec27e20e3d9f
Stanley Sun <stsun@nvidia.com>
2024-11-25 22:31:11 -0800

    [TURTLE]https://nvbugspro.nvidia.com/bug/4616637 Remove medusa V1 mode test

--- tests/llm-backend-test-defs/turtle/defs/test_triton_llm.py
+++ tests/llm-backend-test-defs/turtle/defs/test_triton_llm.py
@@ -592,8 +592,7 @@ def test_llama_v2_70b_ifb(
 @pytest.mark.parametrize("KV_CACHE_FREE_GPU_MEM_FRACTION", [""])
 @pytest.mark.parametrize("ENABLE_TRT_OVERLAP", ["False"],
                          ids=["disableTrtOverlap"])
-@pytest.mark.parametrize("BATCHING_STRATEGY",
-                         ["inflight_fused_batching", "V1"])
+@pytest.mark.parametrize("BATCHING_STRATEGY", ["inflight_fused_batching"])
 @pytest.mark.parametrize("DECOUPLED_MODE", ["True", "False"],
                          ids=["enableDecoupleMode", "disableDecoupleMode"])
 @pytest.mark.parametrize("TRITON_MAX_BATCH_SIZE", ["128"])
